5d7340b9686f263de8c7ea034922b0a7379a83ff
mariamokitashvili <mariam.okitashvili13@gmail.com>
2026-01-29 20:28:01 +0400

    Fix product details dynamic route for Vercel

--- src/app/products/details/[id]/page.jsx
+++ src/app/products/details/[id]/page.jsx
@@ -1,15 +1,8 @@
+export const dynamic = "force-dynamic";
+
 import styles from "./ProductDetails.module.css";
 import AddToCartButton from "@/app/cart/AddToCartButton";
 import { notFound } from "next/navigation";
-
-export async function generateStaticParams() {
-  const res = await fetch("https://fakestoreapi.com/products");
-  const products = await res.json();
-
-  return products.map((product) => ({
-    id: product.id.toString(),
-  }));
-}
 
 async function getProduct(id) {
   const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
@@ -21,7 +14,7 @@ async function getProduct(id) {
 }
 
 export default async function Page({ params }) {
-  const id = params.id;
+  const { id } = await params;
 
   const product = await getProduct(id);
   if (!product) return notFound();
