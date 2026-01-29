d3148e73dbfb29a8dd587f116f6caf339abd9b2a
mariamokitashvili <mariam.okitashvili13@gmail.com>
2026-01-29 22:54:33 +0400

    Fix CSS module case sensitivity for Vercel

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "./products.module.css";
+import styles from "./Products.module.css";
 
 async function getProducts() {
   const res = await fetch("https://fakestoreapi.com/products");
